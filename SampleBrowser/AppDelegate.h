96ec872cfcdc401bd203b998298392eec7eb54ac
Alextnelson <alexander.thomas.nelson@gmail.com>
2016-03-22 23:35:59 -0500

    Initial Commit

---
+++ SampleBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleBrowser
+//
+//  Created by Alexander Nelson on 3/22/16.
+//  Copyright © 2016 Alexander Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleBrowser
+//
+//  Created by Alexander Nelson on 3/22/16.
+//  Copyright © 2016 Alexander Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
